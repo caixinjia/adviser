28d08664da2be0855752724c9409862b3bed612d
unknown <526113132@qq.com>
2018-04-24 00:22:32 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import $api from '../api';
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 // 全局方法
 Vue.prototype.$api = $api;
 new Vue({
