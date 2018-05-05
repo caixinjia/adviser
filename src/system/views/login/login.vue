fcd819829b59a8bc14b25cab473cb903e7d9f325
unknown <526113132@qq.com>
2018-05-05 16:16:17 +0800

    登录样式

--- src/system/views/login/login.vue
+++ src/system/views/login/login.vue
@@ -1,17 +1,23 @@
 <template>
 <div class="login-wrap">
+  <div class="ms-title">高招360后台管理系统</div>
   <div class="ms-login">
     <div class="ms-div" style="margin-top:60px" :class="{on:phoneFocus}">
       <div class="ms-label">账号</div>
       <div class="ms-input">
-        <input @focus="inputFocus(1)" @blur="inputBlur(1)" autocomplete="new-password" />
+        <input @focus="inputFocus(1)" @blur="inputBlur(1)" autocomplete="new-password" v-model="myData.phone" />
       </div>
     </div>
     <div class="ms-div" :class="{on:passwordFocus}">
       <div class="ms-label">密码</div>
       <div class="ms-input">
-        <input type="password" @focus="inputFocus(2)" @blur="inputBlur(2)" autocomplete="new-password" />
+        <input type="password" @focus="inputFocus(2)" @blur="inputBlur(2)" autocomplete="new-password" v-if="hidePwd" v-model="myData.password" />
+        <input @focus="inputFocus(2)" @blur="inputBlur(2)" autocomplete="new-password" v-else v-model="myData.password" />
       </div>
+      <div class="hide-pwd" @click="toHidePwd"></div>
+    </div>
+    <div class="remember" :class="{on:remember}" @click="rememberPwd">
+      <div>记住密码</div>
     </div>
     <div class="login-btn">登录</div>
   </div>
@@ -22,7 +28,13 @@ export default {
   data() {
     return {
       phoneFocus: false,
-      passwordFocus: false
+      passwordFocus: false,
+      hidePwd: true,
+      remember: false,
+      myData: {
+        phone: '',
+        password: ''
+      }
     }
   },
   methods: {
@@ -39,6 +51,12 @@ export default {
       } else {
         this.passwordFocus = false;
       }
+    },
+    toHidePwd() {
+      this.hidePwd = !this.hidePwd;
+    },
+    rememberPwd() {
+      this.remember = !this.remember;
     }
   }
 }
